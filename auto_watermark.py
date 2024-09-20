7742a70a87dd127c2ab460a6e94dc5de8582487c
ParisPosDesenvolvimento <developers@parispos.com.br>
2024-09-20 17:26:06 -0300

    Nessa branch esão alteracoes para software aceitar mxf, seletor por combobox e, por enquanto, só

--- auto_watermark.py
+++ auto_watermark.py
@@ -5,7 +5,7 @@ import re
 import subprocess
 from tkinter import *
 from tkinter import filedialog
-from tkinter.ttk import Progressbar
+from tkinter.ttk import Progressbar, Combobox
 from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 from tqdm import tqdm
 import threading
@@ -14,7 +14,7 @@ ffmpeg_path = 'C:\\Program Files\\ffmpeg-master-latest-win64-gpl\\bin'
 os.environ['PATH'] += os.pathsep + ffmpeg_path
 
 image_extensions = ['.jpg', '.jpeg', '.png']
-video_extensions = ['.mp4','.avi', '.mkv', '.mov']
+video_extensions = ['.mp4','.avi', '.mkv', '.mov', '.mxf']
 root = Tk()
 root.title('Auto Watermark PP')
 
@@ -25,6 +25,9 @@ root.grid_rowconfigure(3, weight=1)
 root.grid_rowconfigure(4, weight=1)
 root.grid_rowconfigure(5, weight=1)
 root.grid_rowconfigure(6, weight=1)
+root.grid_rowconfigure(7, weight=1)
+root.grid_rowconfigure(8, weight=1)
+root.grid_rowconfigure(9, weight=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
@@ -78,12 +81,30 @@ render_title = Text(root, height=1, width=10)
 
 resolution_var = StringVar()
 resolution_var.set('1280x720') 
+framerate_var = StringVar()
+framerate_var.set('23.976')
+formato_var = StringVar()
+formato_var.set('.mov')
+codec_var = StringVar()
+codec_var.set('proxy')
 
-radio_button_1 = Radiobutton(root, text='1280x720 (HD)', value='1280x720', variable=resolution_var)
-radio_button_2 = Radiobutton(root, text='1920x1080 (Full HD)', value='1920x1080', variable=resolution_var)
-radio_button_3 = Radiobutton(root, text='3840x2160 (4K)', value='3840x2160', variable=resolution_var)
+resolution_combo = Combobox(root, textvariable=resolution_var, values=['1280x720', '1920x1080', '3840x2160'])
+resolution_combo.grid(column=0, row=3, padx=5, pady=5, sticky="w")
+resolution_combo.current(0)
 
-progress = Progressbar(root, orient=HORIZONTAL, length=300, mode='indeterminate')
+framerate_combo = Combobox(root, textvariable=framerate_var, values=['23.976', '24', '30'])
+framerate_combo.grid(column=0, row=4, padx=5, pady=5, sticky="w")
+framerate_combo.current(0)
+
+formato_combo = Combobox(root, textvariable=formato_var, values=['.mov', '.mp4'])
+formato_combo.grid(column=0, row=5, padx=5, pady=5, sticky="w")
+formato_combo.current(0)
+
+codec_combo = Combobox(root, textvariable=codec_var, values=['proxy', 'h264'])
+codec_combo.grid(column=0, row=6, padx=5, pady=5, sticky="w")
+codec_combo.current(0)
+
+progress = Progressbar(root, orient=HORIZONTAL, length=300, mode='determinate')
 
 def check_ffmpeg():
 	ffmpeg_path = shutil.which('ffmpeg')
@@ -95,36 +116,72 @@ def check_ffmpeg():
 		exit(1)
 
 def apply_watermark_thread():
-	global image_path, video_path, render_path, ffmpeg_path
-	video_name = str(render_title.get('1.0','end')).strip() + ".mp4"
-	resolution = resolution_var.get()
-	width, height = map(int, resolution.split('x'))
-	render_path = os.path.join(render_path, video_name)
-	ffmpeg_path = check_ffmpeg()
-	command = [
-				ffmpeg_path,
-				'-hwaccel', 'cuda',
-				'-i', video_path,
-				'-i', image_path,
-				'-filter_complex', f'[0:v]scale={width}:{height}[scaled];[scaled][1:v]overlay=W-w-10:H-h-10',
-				'-c:v', 'h264_nvenc', 
-				'-pix_fmt', 'yuv420p',
-				'-preset', 'fast',
-				render_path
-		]
+    global image_path, video_path, render_path, ffmpeg_path
+    codec = codec_var.get()
+    formato = formato_var.get()
+    video_name = str(render_title.get('1.0', 'end')).strip() + formato
+    resolution = resolution_var.get()
+    framerate = framerate_var.get()
+    width, height = map(int, resolution.split('x'))
+    render_path = os.path.join(render_path, video_name)
+    ffmpeg_path = check_ffmpeg()
 
-	try:
-		print("Aplicando marca d'água...")
-		progress.start()
-		subprocess.run(command, check=True)
-		print("Marca d'água aplicada com sucesso!")
-	except subprocess.CalledProcessError as e:
-		print(f"Erro ao aplicar a marca d'água: {e}")
-	except FileNotFoundError as e:
-		print(f"Arquivo não encontrado: {e}")
-	finally:
-		progress.stop()
+    # Initialize command base
+    command = [
+        ffmpeg_path,
+        '-hwaccel', 'cuda',
+        '-i', video_path,
+        '-i', image_path,
+        '-filter_complex', f'[0:v]scale={width}:{height}[scaled];[scaled][1:v]overlay=W-w-10:H-h-10',
+        '-r', framerate,
+    ]
+    
+    # Add codec-specific options
+    if codec == 'h264':
+        command.extend([
+            '-c:v', 'h264_nvenc',
+            '-pix_fmt', 'yuv420p',
+            '-preset', 'fast',
+        ])
+    elif codec == 'proxy':
+        command.extend([
+            '-c:v', 'prores_ks',
+            '-profile:v', '0',
+            '-pix_fmt', 'yuva444p10le',
+        ])
+    else:
+        print(f"Codec não suportado: {codec}")
+        return
 
+    # Add render path
+    command.append(render_path)
+
+    try:
+        print("Aplicando marca d'água...")
+        progress.start()
+        # Open subprocess and capture output
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+        # Read and monitor output
+        for line in process.stderr:
+            if "frame=" in line:
+                # Extract frame count from output
+                frame_info = re.search(r"frame=\s*(\d+)", line)
+                if frame_info:
+                    frame_count = int(frame_info.group(1))
+                    # Update progress (example: assuming 1000 frames)
+                    progress['value'] = (frame_count / frame_count) * 100
+                    root.update_idletasks()
+        
+        process.wait()  # Wait for the process to complete
+        print("Marca d'água aplicada com sucesso!")
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao aplicar a marca d'água: {e}")
+    except FileNotFoundError as e:
+        print(f"Arquivo não encontrado: {e}")
+    finally:
+        progress.stop()
+        
 def apply_watermark():
 	thread = threading.Thread(target=apply_watermark_thread)
 	thread.start()	
@@ -148,14 +205,10 @@ button_destination_folder.grid(column=2, row=1, padx=5, pady=5, sticky="ew")
 render_title_label.grid(column=0, row=2, padx=5, pady=5, sticky="e")
 render_title.grid(column=1, row=2, padx=5, pady=5, columnspan=2, sticky="ew")
 
-radio_button_1.grid(column=0, row=3, padx=5, pady=5, sticky="w")
-radio_button_2.grid(column=1, row=3, padx=5, pady=5, sticky="w")
-radio_button_3.grid(column=2, row=3, padx=5, pady=5, sticky="w")
+button_apply_watermark.grid(column=0, row=7, columnspan=3, padx=5, pady=10, sticky="ew")
 
-button_apply_watermark.grid(column=0, row=4, columnspan=3, padx=5, pady=10, sticky="ew")
+button_exit.grid(column=0, row=9, columnspan=3, padx=5, pady=5, sticky="ew")
 
-button_exit.grid(column=0, row=5, columnspan=3, padx=5, pady=5, sticky="ew")
-
-progress.grid(column=0, row=7, columnspan=3, padx=10, pady=10)
+progress.grid(column=0, row=8, columnspan=3, padx=10, pady=10)
 
 root.mainloop()

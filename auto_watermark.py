500b63fa3ea6f04f7ceff4e54677b05d6d459e7f
bianca terumi <bia.terumi2000@gmail.com>
2024-10-23 15:27:52 -0300

    fix: ajustes de layout para merge

--- auto_watermark.py
+++ auto_watermark.py
@@ -1,5 +1,5 @@
 import os
-import time
+import re
 import shutil
 import subprocess
 from tkinter import *
@@ -87,9 +87,7 @@ def update_selected_paths():
 	selected_paths = f"Imagem: {image_path} \nVídeo: {video_path}\nDestino: {render_path}"
 	selected_paths_label.config(text=selected_paths)
 
-button_image_explore = Button(root, text="Buscar marca d'água", command=browse_image_file)
-button_video_explore = Button(root, text="Buscar vídeo", command=browse_video_file)
-button_destination_folder = Button(root, text="Destino da renderização", command=destination_folder)
+
 selected_paths_label = Label(root, text="Paths Selecionados:")
 
 render_title_label = Label(root, text='Título da renderização:')
@@ -106,25 +104,9 @@ codec_var.set('proxy')
 bitrate_var = StringVar()
 bitrate_var.set('16000')
 
-resolution_combo = Combobox(root, textvariable=resolution_var, values=['1280x720', '1920x1080', '3840x2160'])
-resolution_combo.grid(column=0, row=3, padx=5, pady=5, sticky="w")
-resolution_combo.current(0)
-
-framerate_combo = Combobox(root, textvariable=framerate_var, values=['23.976', '24', '30'])
-framerate_combo.grid(column=0, row=4, padx=5, pady=5, sticky="w")
-framerate_combo.current(0)
-
-formato_combo = Combobox(root, textvariable=formato_var, values=['.mov', '.mp4'])
-formato_combo.grid(column=0, row=5, padx=5, pady=5, sticky="w")
-formato_combo.current(0)
-
-codec_combo = Combobox(root, textvariable=codec_var, values=['proxy', 'h264'])
-codec_combo.grid(column=0, row=6, padx=5, pady=5, sticky="w")
-codec_combo.current(0)
-
 progress = Progressbar(root, orient=HORIZONTAL, length=300, mode='determinate')
-data = filedialog.askdirectory()
-render_path = os.path.abspath(data)
+# data = filedialog.askdirectory()
+# render_path = os.path.abspath(data)
 
 def add_video_to_queue():
 	global video_queue
@@ -244,15 +226,15 @@ def apply_watermark_thread():
 		process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
 		# Read and monitor output
-		# for line in process.stderr:
-		# 	if "frame=" in line:
-		# 		# Extract frame count from output
-		# 		frame_info = re.search(r"frame=\s*(\d+)", line)
-		# 		if frame_info:
-		# 			frame_count = int(frame_info.group(1))
-		# 			# Update progress (example: assuming 1000 frames)
-		# 			progress['value'] = (frame_count / frame_count) * 100
-		# 			root.update_idletasks()
+		for line in process.stderr:
+			if "frame=" in line:
+				# Extract frame count from output
+				frame_info = re.search(r"frame=\s*(\d+)", line)
+				if frame_info:
+					frame_count = int(frame_info.group(1))
+					# Update progress (example: assuming 1000 frames)
+					progress['value'] = (frame_count / frame_count) * 100
+					root.update_idletasks()
 		
 		process.wait()  # Wait for the process to complete
 		print("Marca d'água aplicada com sucesso!")
@@ -272,23 +254,46 @@ def apply_watermark():
 	thread = threading.Thread(target=apply_watermark_thread)
 	thread.start()
 
-button_apply_watermark = Button(root, text="Aplicar", command=apply_watermark)
-button_exit = Button(root, text="Sair", command=root.quit)
-
 # Layout
+
+button_image_explore = Button(root, text="Buscar marca d'água", command=browse_image_file)
+button_video_explore = Button(root, text="Buscar vídeo", command=browse_video_file)
+button_destination_folder = Button(root, text="Destino da renderização", command=destination_folder)
+
+button_add_video = Button(root, text="Adicionar Vídeo à Fila", command=add_video_to_queue)
+button_add_video.grid(column=0, row=2, padx=5, pady=5, sticky="ew")
+
+# Combobox para resolução
+resolution_combo = Combobox(root, textvariable=resolution_var, values=['1280x720', '1920x1080', '3840x2160'])
+resolution_combo.grid(column=0, row=3, padx=5, pady=5, sticky="w")
+resolution_combo.current(0)
+
+# Combobox para taxa de quadros
+framerate_combo = Combobox(root, textvariable=framerate_var, values=['23.976', '24', '30'])
+framerate_combo.grid(column=1, row=3, padx=5, pady=5, sticky="w")
+framerate_combo.current(0)
+
+# Combobox para formato
+format_combo = Combobox(root, textvariable=formato_var, values=['.mov', '.mp4'])
+format_combo.grid(column=0, row=4, padx=5, pady=5, sticky="w")
+format_combo.current(0)
+
+# Combobox para codec
+codec_combo = Combobox(root, textvariable=codec_var, values=['proxy', 'h264'])
+codec_combo.grid(column=1, row=4, padx=5, pady=5, sticky="w")
+codec_combo.current(0)
+
 label_file_explorer.grid(column=0, row=0, columnspan=3, pady=10)
 button_image_explore.grid(column=0, row=1, padx=5, pady=5, sticky="ew")
-button_destination_folder.grid(column=1, row=1, padx=5, pady=5, sticky="ew")
-selected_paths_label.grid(column=0, row=2, padx=5, pady=5, sticky="e")
 
+button_destination_folder.grid(column=1, row=1, padx=5, pady=5, sticky="ew")
 
 render_title_label.grid(column=0, row=2, padx=5, pady=5, sticky="e")
 render_title.grid(column=1, row=2, padx=5, pady=5, columnspan=2, sticky="ew")
 
-selected_paths_label.grid(column=0, row=7, columnspan=3, pady=10)
+selected_paths_label.grid(column=0, row=13, padx=5, pady=5, sticky="e")
 
-button_add_video = Button(root, text="Adicionar Vídeo à Fila", command=add_video_to_queue)
-button_add_video.grid(column=0, row=4, padx=5, pady=5, sticky="ew")
+selected_paths_label.grid(column=0, row=7, columnspan=3, pady=10)
 
 video_listbox = Listbox(root, width=50, height=10)
 video_listbox.grid(column=0, row=5, columnspan=3, padx=10, pady=10)
@@ -296,11 +301,12 @@ video_listbox.grid(column=0, row=5, columnspan=3, padx=10, pady=10)
 button_render_videos = Button(root, text="Renderizar Vídeos", command=render_videos)
 button_render_videos.grid(column=0, row=6, columnspan=3, padx=5, pady=10, sticky="ew")
 progress.grid(column=0, row=8, columnspan=3, padx=10, pady=10)
-button_apply_watermark.grid(column=0, row=9, columnspan=3, padx=5, pady=10, sticky="ew")
-button_exit.grid(column=0, row=10, columnspan=3, padx=5, pady=5, sticky="ew")
 
-button_exit = Button(root, text="Sair", command=exit) 
-button_exit.grid(column=0, row=7, columnspan=3, padx=5, pady=5, sticky="ew")
+button_apply_watermark = Button(root, text="Aplicar", command=apply_watermark)
+button_apply_watermark.grid(column=0, row=9, columnspan=3, padx=5, pady=10, sticky="ew")
+
+button_exit = Button(root, text="Sair", command=root.quit)
+button_exit.grid(column=0, row=10, columnspan=3, padx=5, pady=5, sticky="ew")
 
 load_paths()
 update_selected_paths()
